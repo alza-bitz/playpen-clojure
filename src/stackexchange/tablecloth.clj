c86df128694d95c262014e67617db25efba17136
Alex Coyle <1161048+alza-bitz@users.noreply.github.com>
2025-02-12 01:00:53 +0000

    Switch from scicloj to basecloj template

--- src/stackexchange/tablecloth.clj
+++ src/stackexchange/tablecloth.clj
@@ -3,7 +3,6 @@
    [clj-bom.core :as bom]
    [clojure.data.xml :as xml]
    [clojure.java.io :as io]
-   [clojure.string :as str]
    [hato.client :as hc]
    [tablecloth.api :as tc]))
 
@@ -12,14 +11,14 @@
 (with-open [in (:body (hc/get "https://archive.org/download/stackexchange/astronomy.stackexchange.com.7z"
                               {:as :stream
                                :http-client {:redirect-policy :always}}))
-            out (-> "stackexchange/data/raw/astronomy.stackexchange.com.7z"
+            out (-> "data/stackexchange/raw/astronomy.stackexchange.com.7z"
                     (doto (io/make-parents))
                     io/output-stream)]
   (io/copy in out))
 
 (comment
   (def ^:private reader
-    (bom/bom-reader "stackexchange/data/raw/Users.xml")))
+    (bom/bom-reader "data/stackexchange/raw/Users.xml")))
 
 (comment
   (.close reader))
@@ -42,7 +41,7 @@
 
 ;; 2. Who are the top 10 users with reputation over 1000?
 
-(with-open [users (bom/bom-reader "stackexchange/data/raw/Users.xml")]
+(with-open [users (bom/bom-reader "data/stackexchange/raw/Users.xml")]
   (->
    (sequence (comp (mapcat :content)
                    (map :attrs))
